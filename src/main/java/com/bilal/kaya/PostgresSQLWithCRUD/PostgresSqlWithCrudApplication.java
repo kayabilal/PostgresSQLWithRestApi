3b29af602196f3ec4321634e6af37d507b97c210
kayabilal <kayabilal505@gmail.com>
2022-08-08 14:36:50 +0300

    Initial commit

---
+++ src/main/java/com/bilal/kaya/PostgresSQLWithCRUD/PostgresSqlWithCrudApplication.java
@@ -0,0 +1,13 @@
+package com.bilal.kaya.PostgresSQLWithCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresSqlWithCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresSqlWithCrudApplication.class, args);
+	}
+
+}
